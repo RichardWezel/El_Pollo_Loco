55b3838042f902f7a16bcdd66304fdb0b30a9e64
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-06-11 21:48:25 +0200

    cloud speed variable added

--- models/clouds.class.js
+++ models/clouds.class.js
@@ -1,18 +1,19 @@
 class Cloud extends MovableObject {
     y = 20;
     height = 250;
+    width = 500;
+    speed = 0.15;
 
     constructor() {
         super().loadImage('../img/5_background/layers/4_clouds/1.png');
         
-        this.width = 500;
         this.x = Math.random() * 500;
         this.moveClouds();
     }
 
     moveClouds() {
         setInterval(() => {
-            this.x -= 0.2;
+            this.x -= this.speed;
         }, 1000 / 60);
     }
 }
