901e0b8cbd177e56f33b75255ffb037206a663c8
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-06-13 09:48:55 +0200

    add fuctions to go character right and left and register arrow Keys and space

--- js/game.js
+++ js/game.js
@@ -1,37 +1,57 @@
 let canvas;
 let world;
-
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
+
+
+
+    console.log('My Character is', world.character);
+
 }
 
-addEventListener('keydown', (e) => {
-    console.log(e.code );
-    if (e.code === 'ArrowRight') {
+
+
+// document.addEventListener('keydown', (e) => {
+//     console.log(e)
+// });
+
+window.addEventListener('keydown', (e) => {
+    if (e.code == 'ArrowRight') {
+        keyboard.RIGHT = true;
         world.character.x = world.character.x + 50;
-    } else if (e.code === 'ArrowLeft'){
-        world.character.x = world.character.x - 50;
-    }
+        console.log('keyboard.RIGHT = ', keyboard.RIGHT);
     
+    } else if (e.code === 'ArrowLeft'){
+        keyboard.LEFT = true;
+        world.character.x = world.character.x - 50;
+        console.log('keyboard.LEFT = ', keyboard.LEFT);
+    } else if (e.code === 'ArrowUp') {
+        keyboard.UP = true;
+        console.log('keyboard.UP = ', keyboard.UP);
+    } else if (e.code === 'Space') {
+        keyboard.SPACE = true;
+        console.log('keyboard.SPACE = ', keyboard.SPACE);
+    }
+});
+
+window.addEventListener('keyup', (e) => {
+    if (e.code == 'ArrowRight') {
+        keyboard.RIGHT = false;
+        console.log('keyboard.RIGHT = ', keyboard.RIGHT);
+    
+    } else if (e.code === 'ArrowLeft'){
+        keyboard.LEFT = false;
+        console.log('keyboard.LEFT = ', keyboard.LEFT);
+    } else if (e.code === 'ArrowUp') {
+        keyboard.UP = false;
+        console.log('keyboard.UP = ', keyboard.UP);
+    } else if (e.code === 'Space') {
+        keyboard.SPACE = false;
+        console.log('keyboard.SPACE = ', keyboard.SPACE);
+    }
 });
 
 
-// addEventListener("keyup", function(e) {
-//     if (e.keyCode === 37) {
-//       left()
-//     }
-//   })
-  
-//   addEventListener("keyup", function(e) {
-//     if (e.keyCode === 38) {
-//       jump()
-//     }
-//   })
-  
-//   addEventListener("keyup", function(e) {
-//     if (e.keyCode === 39) {
-//       right()
-//     }
-//   })
--- models/world.class.js
+++ models/world.class.js
@@ -15,15 +15,22 @@ class World {
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 480),// Berge
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0, 300), // Kakten
         new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 400) // bunt Kakten
-    ]
+    ];
     canvas;
     ctx; 
+    keyboard;
 
     // Funktionen
-    constructor(canvas) {
+    constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d'); // Rendering-Kontext
         this.canvas = canvas;
+        this.keyboard = keyboard;
         this.draw();
+        this.setWorld();
+    }
+
+    setWorld() {
+        this.character.world = this;
     }
 
     draw() {
